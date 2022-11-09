b1e8ed44d12f44270acdca290584f25d635f6799
vivek <viveksingh241199@gmail.com>
2022-11-09 10:55:48 +0530

    first

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace WAP1VS_CODE;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
